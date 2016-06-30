27ddee3c8c70f718e5dd1f997e600055bd8172f7
Gabriel Plassard <gabriel.plassard@gmail.com>
2016-06-30 20:04:08 +0200

    step2

--- src/main/scala/net/compiletoi/step2.scala
+++ src/main/scala/net/compiletoi/step2.scala
@@ -4,9 +4,10 @@ import scala.util.parsing.combinator.RegexParsers
 
 
 object CSVParser extends RegexParsers {
-  def cell: Parser[String] = ???
-  def line = ???
-  def csv = ???
+  def regex = "[^\n;]*".r
+  def cell: Parser[String] = regex
+  def line = repsep(regex, ";")
+  def csv = repsep(line, "\n")
 
   def apply(input: String): ParseResult[List[List[String]]] = parseAll(csv, input)
   override def skipWhitespace = false
