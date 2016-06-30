69fbd40e693a9d9cda15040e9a44f77f32c92673
Gabriel Plassard <gabriel.plassard@gmail.com>
2016-06-30 20:03:55 +0200

    step1

--- src/main/scala/net/compiletoi/step1.scala
+++ src/main/scala/net/compiletoi/step1.scala
@@ -4,7 +4,7 @@ import scala.util.parsing.combinator.RegexParsers
 
 
 object Greetings extends RegexParsers {
-  def hello = ???
+  def hello = "Hello " ~> "\\w+".r
 
   def apply(input: String): ParseResult[String] = parseAll(hello, input)
 }
