f06fc9204dbcbbb396e44207b738ef757dc5498f
Gabriel Plassard <gabriel.plassard@gmail.com>
2016-07-09 20:05:01 +0200

    step4

--- src/main/scala/net/compiletoi/step4.scala
+++ src/main/scala/net/compiletoi/step4.scala
@@ -13,14 +13,16 @@ case class JObject(v: Map[String, JSON]) extends JSON
 object JSONParser extends RegexParsers {
   // for some inspiration, see http://www.json.org/
 
-  def str = ???
-  def num = ???
-  def bool = ???
-  def null_ = ???
-  def array = ???
-  def object_ = ???
+  def str: Parser[JStr] = "\"" ~> """[^"]*""".r <~"\"" ^^ JStr
+  def num: Parser[JNum] = "\\d+\\.?\\d*".r ^^ {i => JNum(i.toDouble)}
+  def bool: Parser[JBool] = ("true" | "false") ^^ {b => JBool(b.toBoolean)}
+  def null_ : Parser[JSON] = "null" ^^^ JNull
+  def array: Parser[JArray] = "[" ~> repsep(json, ",") <~ "]" ^^ JArray
+  def item: Parser[(String, JSON)] = str ~ ":" ~ json ^^ { case k ~ _ ~ v => (k.v, v)}
+  def object_ : Parser[JObject] = "{" ~> repsep(item, ",") <~ "}" ^^ {items => JObject(items.toMap)}
 
-  def json: Parser[JSON] = ???
+
+  def json: Parser[JSON] = str | num | bool | null_ | array | object_
 
 
   def apply(input: String): ParseResult[JSON] = parseAll(json, input)
